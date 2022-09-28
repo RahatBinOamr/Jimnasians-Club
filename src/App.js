b445e299ae7ed84808784c121398bd49d08f8817
RahatBinOamr <108364543+RahatBinOamr@users.noreply.github.com>
2022-09-29 05:58:45 +0600

    ans quesiton of question component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Jimnasians from './component/Jimnasians/Jimnasians';
 import Navbar from './component/Navbar/Navbar';
+import Question from './component/Question/Question';
 
 function App() {
   return (
     <div className='App' >
       <Navbar></Navbar>
       <Jimnasians></Jimnasians>
+      <Question></Question>
     </div>
   );
 }
--- src/component/Question/Question.js
+++ src/component/Question/Question.js
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import './Question.css'
 const Question = () => {
     return (
-        <div>
-            <h1> How Does React Work <small>ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code</small> </h1>
+        <div className='question'>
+            <div>
+            <h3> How Does React Work </h3>
+            <small>ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code</small>
+            </div>
+            <div>
+                <h3>Props Vs State</h3>
+                <small>Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components. The this. setState property is used to update the state values in the component</small>
+            </div>
+            <div>
+                <h3>What is React useEffect used for</h3>
+                <small>The useEffect Hook allows you to perform side effects in your components. Some examples of side effects are: fetching data, directly updating the DOM, and timers.</small>
+            </div>
         </div>
     );
 };
