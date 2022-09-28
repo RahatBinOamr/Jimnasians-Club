350d66519356f6086c61d9b153a646a56d289799
RahatBinOamr <108364543+RahatBinOamr@users.noreply.github.com>
2022-09-29 00:56:20 +0600

    added json file

---
+++ src/component/Jimnasian/Jimnasian.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Jimnasian = () => {
+    return (
+        <div>
+            hello iam from single
+        </div>
+    );
+};
+
+export default Jimnasian;
--- src/component/Jimnasians/Jimnasians.js
+++ src/component/Jimnasians/Jimnasians.js
@@ -1,10 +1,20 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import Jimnasian from '../Jimnasian/Jimnasian';
+import './Jimnasians.css'
 const Jimnasians = () => {
-    
+    const [jimnasians,setJimnasians]=useState([]);
+    useEffect(()=>{
+        fetch('data.json')
+        .then(res=>res.json())
+        .then(data=>setJimnasians(data))
+    },[])
     return (
-        <div>
-         
+        <div className='jimnasians-container'>
+         <div> 
+           
+        <Jimnasian jimnasians={jimnasians} ></Jimnasian>
+        </div>
+         <div> store house</div>
             
         </div>
     );
