329cfebc5af11450fb8e8c183bf1e86080147067
RahatBinOamr <108364543+RahatBinOamr@users.noreply.github.com>
2022-09-29 21:27:48 +0600

    change question patten

--- src/component/Question/Question.js
+++ src/component/Question/Question.js
@@ -5,15 +5,15 @@ const Question = () => {
         <div className='question'>
             <div>
             <h3> How Does React Work </h3>
-            <small>ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code</small>
+            <p>ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code</p>
             </div>
             <div>
                 <h3>Props Vs State</h3>
-                <small>Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components. The this. setState property is used to update the state values in the component</small>
+                <p>Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components. The this. setState property is used to update the state values in the component</p>
             </div>
             <div>
                 <h3>What is React useEffect used for</h3>
-                <small>The useEffect Hook allows you to perform side effects in your components. Some examples of side effects are: fetching data, directly updating the DOM, and timers.</small>
+                <p>The useEffect Hook allows you to perform side effects in your components. Some examples of side effects are: fetching data, directly updating the DOM, and timers.</p>
             </div>
         </div>
     );
